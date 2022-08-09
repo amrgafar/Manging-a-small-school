18a350ecace1430bb71b3bae023aa63d3c95930c
Amr Gafar <62294242+amrgafar@users.noreply.github.com>
2022-08-09 21:02:37 +0200

    Update Lec5_Ass2.c

--- Lec5_Ass2.c
+++ Lec5_Ass2.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
+
+\\ Write C code that manage a small school. The school has 3 classes each class
+contains 10 students. Define three arrays for the three classes each one with a
+length of 10. Save a random numbers in all array elements to indicate the
+students grade. The program will calculate and display the following statistics:
+1- Number of passed students
+2- Number of Failed students
+3- Highest grade
+4- Lowest grade
+5- Average grade
+Knowing that the total grade is from 100 and the minimum passing grade is 50.
+
 void main(void)
 {
 	int class1[10],class2[10],class3[10];
